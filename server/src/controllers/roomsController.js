2b37970d18b354169e098efdf37316a02daad648
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-21 14:57:06 -0300

    Add controllers folder and rooms controller

---
+++ server/src/controllers/roomsController.js
@@ -0,0 +1,17 @@
+export default class RoomsController {
+  constructor() {
+    
+  }
+
+  onNewConnection(socket) {
+    const { id } = socket;
+    console.log('connection stablished with', id);
+  }
+
+  getEvents() {
+    const functions = Reflect.ownKeys(RoomsController.prototype)
+      .filter((fn) => fn !== 'constructor')
+      .map((name)=> [name, this[name].bind(this)])
+          
+  }
+}
