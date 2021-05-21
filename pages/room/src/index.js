1f890a14ab62e1f065fdb108bd68fe0de9e7c9bd
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-21 12:51:47 -0300

    Add initial file source for rooms

---
+++ pages/room/src/index.js
@@ -0,0 +1,12 @@
+
+
+const room = {
+  id: Date.now(),
+  topic: 'JS Expert Room',
+};
+
+const user = {
+  img: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/bear_russian_animal_avatar-256.png',
+  username: 'Erick',
+};
+
