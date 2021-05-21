7925b25c7b557ad81cde16703689f1ae0e3e6eca
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-21 13:01:13 -0300

    Create event on individual room

--- pages/room/src/index.js
+++ pages/room/src/index.js
@@ -1,4 +1,15 @@
+import { constants } from '../../_shared/constants.js';
+import SocketBuilder from '../../_shared/sockerBuilder.js';
 
+const socketBuilder = new SocketBuilder({
+  socketUrl: constants.socketUrl,
+  namespace: constants.socketNamespaces.room,
+});
+
+const socket = socketBuilder
+  .setOnUserConnected((user) => console.log('user connected', user))
+  .setOnUserDisconnected((user) => console.log('user disconnected', user))
+  .build();
 
 const room = {
   id: Date.now(),
@@ -10,3 +21,4 @@ const user = {
   username: 'Erick',
 };
 
+socket.emit(constants.events.JOIN_ROOM, { user, room });
