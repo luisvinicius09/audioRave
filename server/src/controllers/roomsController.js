0923a58ba6e48d510d1bd7b81ebb8ab518a64937
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-23 01:44:36 -0300

    Add logic to update global status of the user whenhe enters a room

--- server/src/controllers/roomsController.js
+++ server/src/controllers/roomsController.js
@@ -1,18 +1,55 @@
+import Attendee from '../entities/attendee.js';
 import { constants } from '../util/constants.js';
 
 export default class RoomsController {
+  #users = new Map()
+
   constructor() {
-    
+    this.rooms = new Map();    
   }
 
   onNewConnection(socket) {
     const { id } = socket;
     console.log('connection stablished with', id);
+    this.#updateGlobalUserData(id);
   }
 
-  joinRoom(socket, data) {
-    console.log('data received', data);
-    socket.emit(constants.event.USER_CONNECTED, data)
+  joinRoom(socket, { user, room}) {
+
+    const userId = user.id = socket.id;
+    const roomId = room.id;
+
+    const updatedUserData = this.#updateGlobalUserData(userId, user, roomId);
+
+    console.log({ updatedUserData });
+
+    socket.emit(constants.event.USER_CONNECTED, data);
+  }
+
+  #joinUserRoom(socket, user, room) {
+    const roomId = room.id;
+    const existingRoom = this.rooms.has(roomId);
+    const currentRoom = existingRoom ? this.rooms.get(roomId) : {};
+    const currentUser = new Attendee({
+      ...user,
+      roomId
+    })
+  }
+
+  #updateGlobalUserData(userId, userData = {}, roomId = '') {
+    const user = this.#users.get(userId) ?? {};
+    const existingRoom = this.rooms.has(roomId);
+
+    const updatedUserData = new Attendee({
+      ...user,
+      ...userData,
+      roomId,
+      // if it's the only one in the room
+      isSpeaker: !existingRoom,
+    });
+    this.#users.set(userId, updatedUserData);
+
+    return this.#users.get(userId);
   }
 
   getEvents() {
