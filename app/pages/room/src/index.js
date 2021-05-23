18e16bfdd85d3efec2dc9b6470d2980ad98f560d
Luis Vinicius <luisvinicius0906@gmail.com>
2021-05-23 17:28:45 -0300

    Fix typo

--- app/pages/room/src/index.js
+++ app/pages/room/src/index.js
@@ -12,13 +12,13 @@ const socket = socketBuilder
   .build();
 
 const room = {
-  id: Date.now(),
+  id: '0001',
   topic: 'JS Expert Room',
 };
 
 const user = {
   img: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/bear_russian_animal_avatar-256.png',
-  username: 'Erick',
+  username: 'Erick ' + Date.now()
 };
 
 socket.emit(constants.events.JOIN_ROOM, { user, room });
